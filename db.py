bf50cb16f9db08b2df18a7266a672bd85cfd4a36
ismaelkaissy <ismael.kaissy.56@gmail.com>
2018-08-05 19:16:07 +0100

    setting the database file

--- db.py
+++ db.py
@@ -2,13 +2,12 @@
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
-from flask_sqlalchemy import SQLAlchemy
-
+from .models import *
 
 
 def get_db():
     if 'db' not in g:
-        g.db = SQLAlchemy()
+        g.db = db
         current_app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://root:C0d3rSkad@localhost/vision'
         current_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     return g.db
@@ -17,4 +16,20 @@ def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
-        db.close()
+        db.session.close()
+
+def init_db():
+    db = get_db()
+    db.init_app(current_app)
+    db.create_all()
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    "Clear the existing data and create new tables"
+    init_db()
+    click.echo('initialized the datatbase.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
